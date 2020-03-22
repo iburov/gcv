2b05e884b41240440b369a7245e5828210e699f7
Ivan Burov <iburov.ca@gmail.com>
2020-03-22 02:09:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      HTML
+      <ul>
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+        <li>fourth</li>
+      </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="App">
+      HTML
+      <ul>
+        <address>Home</address>
+        <address>About Us</address>
+        <address>Contact</address>
+        <address>Donate</address>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
